006587aa46cbdc2e5de48d0a767bb85ca2a201d0
Rick Clark <rkingsleyclark@gmail.com>
2017-03-06 14:15:49 +0000

    Add test for transactions being an Array object
    Previous tests only checked that transactions is empty on
    initialization.

--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -5,7 +5,10 @@ describe TransactionLog do
   subject(:transaction_log) { described_class.new }
 
   describe '#transactions' do
-    it 'initializes with an empty transaction array' do
+    it 'initializes as an array' do
+      expect(transaction_log.transactions).to be_an(Array)
+    end
+    it 'initializes as empty' do
       expect(transaction_log.transactions).to be_empty
     end
   end
