b568e19dea727fc472658bec8efa1b7eb3427dd4
Rick Clark <rkingsleyclark@gmail.com>
2017-03-06 12:25:25 +0000

    Add balance instance variable and get method to Account
    Balance is initialised with a value according to the
    INTIAL_BALANCE constant in Account.

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,11 @@
 class Account
 
+  attr_reader :balance
+
+  INITIAL_BALANCE = 0
+
+  def initialize
+    @balance = INITIAL_BALANCE
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,9 @@ require_relative '../lib/account'
 
 describe Account do
 
+  subject(:account) { described_class.new }
+  let(:initial_balance) { described_class::INITIAL_BALANCE }
+
   describe '#initialize' do
     subject(:account_class) { described_class }
     it 'returns a new instance of Account' do
@@ -9,6 +12,12 @@ describe Account do
     end
     it 'does not take any arguments' do
       expect{ account_class.new('arg') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#balance' do
+    it 'initialises with a balance of zero' do
+      expect(account.balance).to eq(initial_balance)
     end
   end
 
