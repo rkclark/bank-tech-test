2a5d7d98b9fcb228b89e5f7d25831ad57dc98e9e
Rick Clark <rkingsleyclark@gmail.com>
2017-03-06 12:06:39 +0000

    Add tests for Account initialisation
    Ensure a new Account can be created, accepting no arguments.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,14 @@ require_relative '../lib/account'
 
 describe Account do
 
+  describe '#initialize' do
+    subject(:account_class) { described_class }
+    it 'returns a new instance of Account' do
+      expect(account_class.new).to be_a(account_class)
+    end
+    it 'does not take any arguments' do
+      expect{ account_class.new('arg') }.to raise_error(ArgumentError)
+    end
+  end
+
 end
