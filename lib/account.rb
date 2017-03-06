6a3ca36e603384670360dd8b35909be917dd1580
Rick Clark <rkingsleyclark@gmail.com>
2017-03-06 16:17:27 +0000

    Update Account to validate transaction_log_class is a Class

--- lib/account.rb
+++ lib/account.rb
@@ -5,6 +5,8 @@ class Account
   INITIAL_BALANCE = 0
 
   def initialize(transaction_log_class: TransactionLog)
+    type_error = 'The transaction_log_class argument can only accept a Class object'
+    raise type_error unless transaction_log_class.is_a?(Class)
     @balance = INITIAL_BALANCE
   end
 
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,19 +3,26 @@ require_relative '../lib/transaction_log'
 
 describe Account do
 
-  let(:transaction_log_class) { class_double('TransactionLog') }
+  let(:transaction_log_class) { TransactionLog }
   subject(:account) { described_class.new }
   let(:initial_balance) { described_class::INITIAL_BALANCE }
 
 
   describe '#initialize' do
     subject(:account_class) { described_class }
-    it 'can accept a transaction_log_class keyword argument' do
-      expect{ account_class.new(transaction_log_class: transaction_log_class) }.not_to raise_error
+    describe 'transaction_log_class keyword argument' do
+      it 'accepts a transaction_log_class keyword argument' do
+        expect{ account_class.new(transaction_log_class: transaction_log_class) }.not_to raise_error
+      end
+      it 'can initialize without the transaction_log_class keyword argument' do
+        expect{ account_class.new }.not_to raise_error
+      end
+      it 'raises error if the argument receives an object that is not a class' do
+        type_error = 'The transaction_log_class argument can only accept a Class object'
+        expect{ account_class.new(transaction_log_class: '') }.to raise_error(type_error)
+      end
     end
-    it 'can initialize without the transaction_log_class keyword argument' do
-      expect{ account_class.new }.not_to raise_error
-    end
+
   end
 
   describe '#balance' do
