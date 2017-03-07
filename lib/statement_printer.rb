91b507401d9722f90c3edd2efc567bb144458684
Rick Clark <rkingsleyclark@gmail.com>
2017-03-07 14:43:32 +0000

    Add #print_out method to Statement_Printer
    New #print_out method takes an account as argument and prints the
    statement summary to STDOUT in the specified format.
    
    Add Transaction Struct to Statement_Printer which is used by
    new #prepare_data private method to put transform Transaction
    objects into easily usable key/value string pairs.
    
    Add #stringify_transactions private method to convert each
    Transcation Struct into a transaction string with the required
    left and right adjustments applied.

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,3 +1,35 @@
 class StatementPrinter
 
+  def print_out(account)
+    transactions = prepare_data(account)
+    output_string = header_string + stringify_transactions(transactions)
+    print output_string
+  end
+
+  private
+
+  Transaction = Struct.new(:date, :credit, :debit, :balance)
+
+  def prepare_data(account)
+    total_balance = account.class::INITIAL_BALANCE
+    account.transaction_log.transactions.map do |t|
+      date = t.date.strftime("%d/%m/%Y")
+      credit = t.amount > 0 ?  "#{sprintf "%.2f", t.amount}" : ""
+      debit = t.amount < 0 ?  "#{sprintf "%.2f", t.amount.abs}" : ""
+      balance = "#{sprintf "%.2f", total_balance += t.amount}"
+      Transaction.new(date, credit, debit, balance)
+    end.reverse
+  end
+
+  def header_string
+    "date       || credit || debit   || balance\n"
+  end
+
+  def stringify_transactions(transactions)
+    transactions_string = ''
+    transactions.each do |t|
+      transactions_string += "#{t.date.ljust(11)}||#{t.credit.rjust(8)}|| #{t.debit.ljust(8)}||#{t.balance.rjust(8)}\n"
+    end
+    transactions_string.chomp
+  end
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -1,5 +1,41 @@
 require_relative '../lib/statement_printer'
+require_relative '../lib/transaction'
+require_relative '../lib/transaction_log'
+require_relative '../lib/account'
+require 'date'
 
 describe StatementPrinter do
-  
+
+  let(:transaction_1) { instance_double('Transaction') }
+  let(:transaction_2) { instance_double('Transaction') }
+  let(:transaction_3) { instance_double('Transaction') }
+  let(:transaction_log) { instance_double('TransactionLog') }
+  let(:account) { instance_double('Account') }
+  let(:account_class) { class_double('Account').as_stubbed_const(:transfer_nested_constants => true) }
+
+  subject(:statement_printer) { described_class.new }
+
+  let(:output_string) {
+    "date       || credit || debit   || balance
+14/01/2012 ||        || 500.00  || 2500.00
+13/01/2012 || 2000.00||         || 3000.00
+10/01/2012 || 1000.00||         || 1000.00"
+  }
+
+  before do
+    allow(transaction_1).to receive_messages(date: DateTime.new(2012,1,10), amount: 1000.00)
+    allow(transaction_2).to receive_messages(date: DateTime.new(2012,1,13), amount: 2000.00)
+    allow(transaction_3).to receive_messages(date: DateTime.new(2012,1,14), amount: -500.00)
+    allow(transaction_log).to receive(:transactions) { [ transaction_1, transaction_2, transaction_3 ] }
+    allow(account).to receive(:transaction_log) { transaction_log }
+    allow(account).to receive(:class) { account_class }
+
+  end
+
+  describe '#print' do
+    it 'prints the statement in the correct format' do
+      expect{ statement_printer.print_out(account) }.to output(output_string).to_stdout
+    end
+  end
+
 end
